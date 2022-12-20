169e3ba415b04f58a116c71b259f23830eb97b01
moudjane <mathis.oudjane@gmail.com>
2022-12-20 03:40:02 +0100

    '/' for space and ' ' for each letter + CSS

--- src/components/LatinToMorse.vue
+++ src/components/LatinToMorse.vue
@@ -7,7 +7,7 @@ export default {
   },
   computed: {
     convert() {
-      return this.latin.split('').map(char => this.abc(char)).join('/');
+      return this.latin.split('').map(char => this.abc(char)).join(' ');
     },
   },
   methods: {
@@ -52,8 +52,9 @@ export default {
         '.': '.-.-.-',
         ',': '--..--',
         '?': '..--..',
-        // '/': '-..-.',
+        '/': '-..-.',
         '@': '.--.-.',
+        ' ': '/'
       };
       char = char.toUpperCase();
       return LatinToMorse[char] || '';
@@ -63,15 +64,15 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <div>
       <h1>Latin to morse converter:</h1>
     </div>
     <div>
-      <input v-model="latin" id="latin" placeholder="write here">
+      <input class="latin-input" v-model="latin" placeholder="write here" maxlength="30" />
     </div>
-    <div>
-      Latin text: <span class="latin"> {{ latin }}</span>
+    <div class="limit">
+      Latin <span class="latin"> {{ latin }}</span>
       <br />
       Morse: <span class="morse"> {{ convert }}</span>
     </div>
@@ -85,5 +86,37 @@ export default {
 
 .morse {
   color: rgb(255, 136, 136);
+}
+
+/* .container {
+  border: 5px solid white;
+  padding: 0.125rem 0.5rem;
+
+  width: 50%;
+  height: 75%;
+  overflow-wrap: break-word;
+} */
+.latin-input {
+
+  width: fit-content;
+  border: 0;
+  border-bottom: 1.5px solid var(--color-text);
+  outline: 0;
+
+  color: var(--color-text);
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.latin-input:focus {
+  padding-bottom: 6px;
+  font-size: 17px;
+  border-width: 3px;
+  border-image: linear-gradient(to right, rgb(132, 132, 255), rgb(255, 136, 136));
+  border-image-slice: 1;
 }
 </style>
