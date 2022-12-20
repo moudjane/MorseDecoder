d63065733cb7618e0f976394c44e96da13c4f95a
moudjane <mathis.oudjane@gmail.com>
2022-12-20 02:13:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import LatinToMorse from "./components/LatinToMorse.vue"
+export default {
+  data() {
+    return {
+    }
+  },
+  components: {
+    LatinToMorse: LatinToMorse
+  }
+}
+</script>
+
+<template>
+  <LatinToMorse />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/LatinToMorse.vue
@@ -0,0 +1,89 @@
+<script>
+export default {
+  data() {
+    return {
+      latin: '',
+    }
+  },
+  computed: {
+    convert() {
+      return this.latin.split('').map(char => this.abc(char)).join('/');
+    },
+  },
+  methods: {
+    abc(char) {
+      const LatinToMorse = {
+        'A': '.-',
+        'B': '-...',
+        'C': '-.-.',
+        'D': '-..',
+        'E': '.',
+        'F': '..-.',
+        'G': '--.',
+        'H': '....',
+        'I': '..',
+        'J': '.---',
+        'K': '-.-',
+        'L': '.-..',
+        'M': '--',
+        'N': '-.',
+        'O': '---',
+        'P': '.--.',
+        'Q': '--.-',
+        'R': '.-.',
+        'S': '...',
+        'T': '-',
+        'U': '..-',
+        'V': '...-',
+        'W': '.--',
+        'X': '-..-',
+        'Y': '-.--',
+        'Z': '--..',
+        '1': '.----',
+        '2': '..---',
+        '3': '...--',
+        '4': '....-',
+        '5': '.....',
+        '6': '-....',
+        '7': '--...',
+        '8': '---..',
+        '9': '----.',
+        '0': '-----',
+        '.': '.-.-.-',
+        ',': '--..--',
+        '?': '..--..',
+        // '/': '-..-.',
+        '@': '.--.-.',
+      };
+      char = char.toUpperCase();
+      return LatinToMorse[char] || '';
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <div>
+      <h1>Latin to morse converter:</h1>
+    </div>
+    <div>
+      <input v-model="latin" id="latin" placeholder="write here">
+    </div>
+    <div>
+      Latin text: <span class="latin"> {{ latin }}</span>
+      <br />
+      Morse: <span class="morse"> {{ convert }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.latin {
+  color: rgb(132, 132, 255);
+}
+
+.morse {
+  color: rgb(255, 136, 136);
+}
+</style>
